77f98dd324225f3ef0328cd8ae7baaa805120ac9
root <root@DESKTOP-S1LREOS.>
2025-09-16 22:41:22 +0200

    ok

--- math/linear_algebra/14-saddle_up.py
+++ math/linear_algebra/14-saddle_up.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
-"""Module that provides a function to perform matrix multiplication
 """
+Performs matrix multiplication on two numpy arrays.
+"""
+import numpy as np
+
 
 def np_matmul(mat1, mat2):
-    """Performs matrix multiplication of two 2D matrices (pure Python)"""
-    if len(mat1[0]) != len(mat2):
-        return None
-    return [
-        [sum(a * b for a, b in zip(row, col)) for col in zip(*mat2)]
-        for row in mat1
-    ]
+    """Return the matrix product of mat1 and mat2"""
+    return np.matmul(mat1, mat2)
