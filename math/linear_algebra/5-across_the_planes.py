9d1d54329b377468e135c64004969a4b77d5a863
root <root@DESKTOP-S1LREOS.>
2025-09-16 21:58:31 +0200

    ok

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 """
-Module for adding two 2D matrices element-wise
+adding two 2D matrices element-wise
 """
+
 
 def add_matrices2D(mat1, mat2):
     """Adds two 2D matrices
     """
-    if (len(mat1) != len(mat2) or
-        any(len(row1) != len(row2) for row1, row2 in zip(mat1, mat2))):
+    if (len(mat1) != len(mat2)
+            or any(len(row1) != len(row2)
+                   for row1, row2 in zip(mat1, mat2))):
         return None
-    return [[a + b for a, b in zip(row1, row2)] for row1, row2 in zip(mat1, mat2)]
+
+    return [[a + b for a, b in zip(row1, row2)]
+            for row1, row2 in zip(mat1, mat2)]
