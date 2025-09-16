63bd25a327c24c7ff76e38742bafaae1ff55f5b5
root <root@DESKTOP-S1LREOS.>
2025-09-16 22:34:01 +0200

    ok

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -1,20 +1,8 @@
 #!/usr/bin/env python3
-"""
-Module that provides a function to perform element-wise operations on matrices
-"""
-
-
 def np_elementwise(mat1, mat2):
-    """
-    Perform element-wise addition, subtraction, multiplication, and division
-    """
-    # Si mat2 est un entier ou float, on le transforme en "matrice" de même taille que mat1
-    if isinstance(mat2, (int, float)):
-        mat2 = [[mat2 for _ in row] for row in mat1]
-
-    add = [[a + b for a, b in zip(r1, r2)] for r1, r2 in zip(mat1, mat2)]
-    sub = [[a - b for a, b in zip(r1, r2)] for r1, r2 in zip(mat1, mat2)]
-    mul = [[a * b for a, b in zip(r1, r2)] for r1, r2 in zip(mat1, mat2)]
-    div = [[a / b for a, b in zip(r1, r2)] for r1, r2 in zip(mat1, mat2)]
-
-    return (add, sub, mul, div)
+    """Return element-wise sum, difference, product, and quotient"""
+    add = mat1 + mat2
+    sub = mat1 - mat2
+    mul = mat1 * mat2
+    div = mat1 / mat2
+    return add, sub, mul, div
