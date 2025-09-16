b4debffe3910ead9ebd04118c45748fc2789f1f1
root <root@DESKTOP-S1LREOS.>
2025-09-16 22:29:53 +0200

    ok

--- math/linear_algebra/11-the_western_exchange.py
+++ math/linear_algebra/11-the_western_exchange.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env python3
-"""
-Module that provides a function to get the transpose of a matrix
-"""
+"""Module that provides a function to transpose a numpy.ndarray"""
 
 
 def np_transpose(matrix):
-    """Return the transpose of a matrix (list of lists)"""
-    if not matrix:
-        return []
-    return [list(row) for row in zip(*matrix)]
+    """Return a new numpy.ndarray that is the transpose of matrix"""
+    return matrix.T
