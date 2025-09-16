e293d76d628dc231ba5afbf6e2c41ad9e941b9b8
root <root@DESKTOP-S1LREOS.>
2025-09-16 22:36:53 +0200

    ok

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-"""Module that provides a function to perform element-wise operations on two matrices
+"""provides a function to perform element-wise operations on two matrices
 """
 
 
 def np_elementwise(mat1, mat2):
-    """Return element-wise sum, difference, product, and quotient"""
+    """Return sum, difference, product, and quotient"""
     add = mat1 + mat2
     sub = mat1 - mat2
     mul = mat1 * mat2
