5532f26b96ecc00d83c804522433d7035d78b4b3
root <root@DESKTOP-S1LREOS.>
2025-09-16 22:24:13 +0200

    ok

--- math/linear_algebra/10-ill_use_my_scale.py
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -1,15 +1,4 @@
 #!/usr/bin/env python3
-"""
-Module that provides a function to get the shape of a matrix as a tuple
-"""
-
-
 def np_shape(matrix):
-    """Return the shape of a matrix (nested lists) as a tuple"""
-    shape = []
-    while isinstance(matrix, list):
-        shape.append(len(matrix))
-        if len(matrix) == 0:
-            break
-        matrix = matrix[0]
-    return tuple(shape)
+    """Return the shape of a numpy.ndarray as a tuple of integers"""
+    return matrix.shape
